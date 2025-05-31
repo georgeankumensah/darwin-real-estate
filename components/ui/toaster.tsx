6be0cd0973d9c60b8fc4f86843ba8d452c4de716
Kelvin Ampomah Boateng <kaboateng29@st.knust.edu.gh>
2025-05-31 19:07:59 +0000

    fix: imports

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,8 +1,14 @@
 "use client"
 
-import {useToast} from "@/hooks/use-toast"
-import {Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport} from "@radix-ui/react-toast"
-
+import {useToast} from "@/hooks/use-toast";
+import {
+    Toast,
+    ToastClose,
+    ToastDescription,
+    ToastProvider,
+    ToastTitle,
+    ToastViewport,
+} from "@/components/ui/toast"
 
 export function Toaster() {
     const {toasts} = useToast()
