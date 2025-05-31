a6aa61722004b8ffc5de81e8fb2a0ae880ac0680
Kelvin Ampomah Boateng <kaboateng29@st.knust.edu.gh>
2025-05-31 19:09:32 +0000

    feat: add query provider and toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { DashboardSidebar } from "@/components/layout/sidebar";
+import QueryProvider from "@/components/provider/QueryProvider";
+import {Toaster} from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Darwin Real Estate",
-  description: "Find your dream home with Darwin Real Estate",
+    title: "Darwin Real Estate",
+    description: "Find your dream home with Darwin Real Estate",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <main className="flex-1 ">{children}</main>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <QueryProvider>
+            <main className="flex-1 ">{children}</main>
+            <Toaster/>
+        </QueryProvider>
+        </body>
+        </html>
+    );
 }
