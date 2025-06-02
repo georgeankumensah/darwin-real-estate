3c4df2ef0b74bb2dc31ece454acd38e11464b0af
georgeankumensah <georgeankumensah@gmail.com>
2025-06-02 14:53:07 +0000

    fix(ui): create transaction form

--- app/(admin)/transactions/page.tsx
+++ app/(admin)/transactions/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
           </p>
         </div>
         <Link
-          href="/properties/new"
+          href="/transactions/new"
           className="flex items-center gap-2 bg-primary text-sm rounded-[6px] text-white px-4 py-2  hover:bg-primary/90 transition-colors"
         >
           <Plus className="h-6 w-6 text-white" />
